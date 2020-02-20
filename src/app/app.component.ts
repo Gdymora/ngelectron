0c4255e5960e2819611d9fcf1e90ff607aafbe4d
Alexandr <alekspetrofint@gmail.com>
2020-02-20 19:48:14 +0200

    modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { IpcRenderer } from 'electron';
 
+//npm run start:electron
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngelectron';
+  private ipc: IpcRenderer
+      constructor(){
+        if ((<any>window).require) {
+          try {
+            this.ipc = (<any>window).require('electron').ipcRenderer;
+          } catch (e) {
+            throw e;
+          }
+        } else {
+          console.warn('App not running inside Electron!');
+        }
+      }
+
+      openModal(){
+        console.log("Open a modal");
+        this.ipc.send("openModal");
+      }
 }
